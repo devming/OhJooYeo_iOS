ab524ad750914887f9853f6e07033493d8db142e
devming <cmk330@gmail.com>
2023-01-29 16:27:33 +0900

    Initial Commit

---
+++ Ohjooyeo/OhjooyeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OhjooyeoApp.swift
+//  Ohjooyeo
+//
+//  Created by devming on 2023/01/29.
+//
+
+import SwiftUI
+
+@main
+struct OhjooyeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
