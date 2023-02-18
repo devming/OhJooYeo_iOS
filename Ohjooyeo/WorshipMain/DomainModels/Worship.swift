2dc4ca75852deab63a82c73a5781ffc5b56edd32
devming <cmk330@gmail.com>
2023-02-18 17:14:03 +0900

    도메인 모델 추가

---
+++ Ohjooyeo/WorshipMain/DomainModels/Worship.swift
@@ -0,0 +1,14 @@
+//
+//  Worship.swift
+//  Ohjooyeo
+//
+//  Created by devming on 2023/02/18.
+//
+
+import Foundation
+
+struct Worship {
+    let id: Int
+    let name: String
+    let date: String
+}
